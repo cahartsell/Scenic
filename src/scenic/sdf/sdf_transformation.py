9180bdd3dd0850e499fd405ed4ddf7a9d4c4c20e
Madison McClellan <madisonemcc@gmail.com>
2019-06-19 11:49:05 -0500

    Added geometry and material elements to model template

--- src/scenic/sdf/sdf_transformation.py
+++ src/scenic/sdf/sdf_transformation.py
@@ -13,7 +13,8 @@ model_template = env.get_template('model_template')
 with open('world_data.json') as json_file:
     data = json.load(json_file)
     all_models = ''
-    for x in data['world']:
-        all_models = all_models + model_template.render(model_name=x['name']) + '\n\n'
+    for m in data['world']:
+        all_models = all_models + model_template.render(model_name=m['name'], x=m['x'], y=m['y'], z=m['z'], roll='0',
+                                                        pitch='0', yaw='0', shape_type='box', color=m['color']) + '\n\n'
     with open('example.sdf', 'w+') as write_file:
-        write_file.write(world_template.render(world_name='world', model=all_models))
+        write_file.write(world_template.render(world_name='Example World', models=all_models))
